076d5a6e2bedf515a0a7370ffee094bd25d963c2
Raissa <raissateixeir4@gmail.com>
2026-04-20 15:58:26 -0300

    Feat: Adicionando um display

--- src/main.py
+++ src/main.py
@@ -1,7 +1,84 @@
 
-from machine import Pin, PWM
+from machine import Pin, PWM, I2C
+from micropython import const
+import framebuf
 import time
-import sys
+
+# código pro display funcionar, pois não consegui importa a biblioteca
+SET_CONTRAST = const(0x81)
+SET_ENTIRE_ON = const(0xa4)
+SET_NORM_INV = const(0xa6)
+SET_DISP = const(0xae)
+SET_MEM_ADDR = const(0x20)
+SET_COL_ADDR = const(0x21)
+SET_PAGE_ADDR = const(0x22)
+SET_DISP_START_LINE = const(0x40)
+SET_SEG_REMAP = const(0xa0)
+SET_MUX_RATIO = const(0xa8)
+SET_COM_OUT_DIR = const(0xc0)
+SET_DISP_OFFSET = const(0xd3)
+SET_COM_PIN_CFG = const(0xda)
+SET_DISP_CLK_DIV = const(0xd5)
+SET_PRECHARGE = const(0xd9)
+SET_VCOM_DESEL = const(0xdb)
+SET_CHARGE_PUMP = const(0x8d)
+
+class SSD1306_I2C:
+    def __init__(self, width, height, i2c, addr=0x3C):
+        self.width = width
+        self.height = height
+        self.i2c = i2c
+        self.addr = addr
+        self.buffer = bytearray(self.height // 8 * self.width)
+        self.framebuf = framebuf.FrameBuffer(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
+        self.init_display()
+
+    def write_cmd(self, cmd):
+        self.i2c.writeto(self.addr, bytearray([0x80, cmd]))
+
+    def write_data(self, buf):
+        self.i2c.writeto(self.addr, b'\x40' + buf)
+
+    def init_display(self):
+        for cmd in (
+            SET_DISP | 0x00,
+            SET_MEM_ADDR, 0x00,
+            SET_DISP_START_LINE | 0x00,
+            SET_SEG_REMAP | 0x01,
+            SET_MUX_RATIO, self.height - 1,
+            SET_COM_OUT_DIR | 0x08,
+            SET_DISP_OFFSET, 0x00,
+            SET_COM_PIN_CFG, 0x12,
+            SET_DISP_CLK_DIV, 0x80,
+            SET_PRECHARGE, 0xf1,
+            SET_VCOM_DESEL, 0x30,
+            SET_CONTRAST, 0xff,
+            SET_ENTIRE_ON,
+            SET_NORM_INV,
+            SET_CHARGE_PUMP, 0x14,
+            SET_DISP | 0x01,
+        ):
+            self.write_cmd(cmd)
+
+    def fill(self, col):
+        self.framebuf.fill(col)
+
+    def text(self, string, x, y):
+        self.framebuf.text(string, x, y)
+
+    def show(self):
+        self.write_cmd(SET_COL_ADDR)
+        self.write_cmd(0)
+        self.write_cmd(self.width - 1)
+        self.write_cmd(SET_PAGE_ADDR)
+        self.write_cmd(0)
+        self.write_cmd(self.height // 8 - 1)
+        self.write_data(self.buffer)
+
+#display
+i2c = I2C(0, scl=Pin(22), sda=Pin(15))
+oled = SSD1306_I2C(128, 64, i2c)
+
 
 # LEDs
 led_azul = Pin(4, Pin.OUT)
@@ -17,11 +94,19 @@ botao_verde = Pin(19, Pin.IN, Pin.PULL_UP)
 buzzer = PWM(Pin(22))
 buzzer.duty(0)
 
+#função pra mostrar a mensagem no display
+def mostrar_mensagem(l1, l2=""):
+    oled.fill(0)
+    oled.text(l1, 0, 0)
+    oled.text(l2, 0, 20)
+    oled.show()
+
+#desliga todos os leds
 def desligar_todos():
     led_azul.off()
     led_vermelho.off()
     led_verde.off()
-
+#aciona o led, depois desliga e liga 5 vezes
 def acionar(led):
     desligar_todos()
     led.on()
@@ -54,20 +139,23 @@ def som_urgente():  # ajuda
         time.sleep(0.10)
 
 def acao_ajuda():
+    mostrar_mensagem("PRECISO DE", "AJUDA")
     som_urgente()
     acionar(led_vermelho)
     print("Ação: Preciso de ajuda")
 
 def acao_banheiro():
+    mostrar_mensagem("IR AO", "BANHEIRO")
     som_curto()
     acionar(led_azul)
     print("Ação: Ir ao banheiro")
 
 def acao_ok():
+    mostrar_mensagem("ESTOU", "BEM")
     som_longo()
     acionar(led_verde)
     print("Ação: Estou bem")
-
+#função para simular o teste automático 
 def modo_teste():
     print("Iniciando teste automático")
 
@@ -76,8 +164,6 @@ def modo_teste():
     acao_ok()
 
     print("SIMULACAO_OK")
-    time.sleep(1)
-    sys.exit()
 
 modo_teste()
         
